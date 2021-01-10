b3370446688709a77b3fcbb53b4a18632feed84c
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-01-09 23:52:38 -0500

    added server code, added models code.

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: { type: Date, default: Date.now()
+    },
+    exercises: [
+      {
+        name: String,
+        distance: Number,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number,
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
---
+++ server.js
@@ -0,0 +1,39 @@
+// modules 
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+// port 
+const PORT = process.env.PORT || 8080;
+
+// set middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose connected successfully.");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: " + err);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
